6717c97cc7c08804aa14464aa663eed762cd21cd
Fernando Garza <nndo1991@gmail.com>
2013-07-24 12:09:17 -0500

    initial commit of Pinterest-like app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pinterest::Application.config.secret_token = '9953269f3f4535715d66f7b58076fef7d422605b0f15cda5e48516973f30dcb25af9db0d141d5e930285a21eae621e6449075433c6c021aa990154f39436d13d'
